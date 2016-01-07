1de6c81b338ebd27644c597f1f2623ef46f7e16a
SimonLab <simon.labondance@gmail.com>
2016-01-07 14:32:17 +0000

    delete the \r for each value, https://github.com/akitten/vcard-linkedin-parser/issues/6

--- index.js
+++ index.js
@@ -9,23 +9,25 @@ module.exports = function(vcards) {
     }
 
     if(line.startsWith("N:")) {
-      result[currentIndex].names = line.substr(2).split(';');
+      result[currentIndex].names = line.substr(2).split(';').map(function(name) {
+        return name.trim();
+      });
     }
 
     if(line.startsWith('FN:')) {
-      result[currentIndex].fullname =  line.substr(3);
+      result[currentIndex].fullname =  line.substr(3).trim();
     }
 
     if(line.startsWith('TITLE:')) {
-      result[currentIndex].headline = line.substr(6);
+      result[currentIndex].headline = line.substr(6).trim();
     }
 
     if(line.startsWith('ORG:')) {
-      result[currentIndex].org = line.substr(4);
+      result[currentIndex].org = line.substr(4).trim();
     }
 
     if(line.startsWith("EMAIL;INTERNET:")) {
-      result[currentIndex].email = line.substr(15);
+      result[currentIndex].email = line.substr(15).trim();
     }
 
     if(line.startsWith("END:VCARD")){
